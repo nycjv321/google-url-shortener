0437c8a9d47581f127b8c664454a82bd58c41887
Javier L. Velasquez <nycjv321@gmail.com>
2012-12-21 23:52:02 -0500

    Move HTTP Client Code to Utility Class

--- src/googleurlshortener/GoogleURLShortener.java
+++ src/googleurlshortener/GoogleURLShortener.java
@@ -5,18 +5,9 @@
 package googleurlshortener;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
 import org.json.JSONException;
 import org.json.JSONObject;
+import utility.system.HttpClientUtility;
 
 /**
  *
@@ -27,75 +18,92 @@ public class GoogleURLShortener {
     /**
      * @param args the command line arguments
      */
-    private String status;
     private String id;
     private String url;
-    private DefaultHttpClient httpclient = new DefaultHttpClient();
-    private HttpPost httpPost = new HttpPost("https://www.googleapis.com/urlshortener/v1/url");
+    private String googleURL;
+    private HttpClientUtility httpclient;
 
     public static void main(String[] args) throws IOException, JSONException {
         GoogleURLShortener gus = new GoogleURLShortener();
+
+
         gus.calculateID("http://google.com");
         System.out.println(gus.getId());
 
         GoogleURLShortener gus2 = new GoogleURLShortener("https://www.facebook.com/nycjv321");
         gus2.calculateID();
-        System.out.println(gus2.getId());
+        System.out.println(gus2.getUrl());
+        gus2.setId("http://goo.gl/fbsS");
+        gus2.calculateURL();
+        System.out.println(gus2.getUrl());
+
 
     }
 
     GoogleURLShortener(String url) {
+        this.init();
         this.url = url;
     }
 
     GoogleURLShortener() {
+        this.init();
     }
 
+    public void init() {
+        this.googleURL = "https://www.googleapis.com/urlshortener/v1/url";
+        this.httpclient = new HttpClientUtility();
+    }
+    
     public void calculateID() throws IOException, JSONException {
 
 
-        // Define HTTP Post Entity        
-        StringEntity stringentity = new StringEntity(
-                "{\"longUrl\": \"" + this.url + "\"}",
-                ContentType.create("application/json", "UTF-8"));
-        // Set Entity in Post Object
-        this.httpPost.setEntity(stringentity);
-        // Perform Post
-        HttpResponse response2 = this.httpclient.execute(this.httpPost);
-        // Get Status of Post
-        this.setStatus(String.valueOf(response2.getStatusLine()));
-        // Get Output Stream
-        HttpEntity httpentity = response2.getEntity();
+        String output = httpclient.performPost(
+                this.getGoogleURL(),
+                "{\"longUrl\": \"" + this.getUrl() + "\"}", "application/json",
+                "UTF-8");
 
-        String output = EntityUtils.toString(httpentity);
 
         // Lets use JSON Library to interact with String (Lazy Much?)
         JSONObject jsonobject = new JSONObject(output);
         // Lets save the field
         this.setId(jsonobject.getString("id"));
-        // Close Stream
-        EntityUtils.consume(httpentity);
-        // Disconnect
-        this.httpPost.releaseConnection();
     }
 
     public void calculateID(String url) throws IOException, JSONException {
-        this.url = url;
+        this.setUrl(url);
         this.calculateID();
     }
 
-    /**
-     * @return the status
-     */
-    public String getStatus() {
-        return status;
+    public void calculateURL() throws IOException, JSONException {
+
+
+
+        String output = httpclient.performGet(this.googleURL + "?shortUrl=" + this.getId());
+
+
+        // Lets use JSON Library to interact with String (Lazy Much?)
+        JSONObject jsonobject = new JSONObject(output);
+        // Lets save the field
+        this.setUrl(jsonobject.getString("longUrl"));
+    }
+
+    public void calculateURL(String id) throws IOException, JSONException {
+        this.setId(id);
+        this.calculateURL();
     }
 
     /**
-     * @param status the status to set
+     * @return the url
      */
-    public void setStatus(String status) {
-        this.status = status;
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     /**
@@ -110,5 +118,12 @@ public class GoogleURLShortener {
      */
     public void setId(String id) {
         this.id = id;
+    }
+
+    /**
+     * @return the googleURL
+     */
+    public String getGoogleURL() {
+        return googleURL;
     }
 }
